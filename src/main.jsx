081c50b1ec782daa2d2258340e9c9d2ce7e32348
탭・TAB・タブ <me@tabthedev.kr>
2026-07-16 14:32:41 +0900

    이름 변경 미반영 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Root from './pages/root.jsx'
+import Root from './pages/Root.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
