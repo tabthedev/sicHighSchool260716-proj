6d409762fa834b8dfbce79ca506e3d0e43653d2a
탭・TAB・タブ <me@tabthedev.kr>
2026-07-17 19:34:09 +0900

    header에 리포지토리 링크 추가

--- src/pageFormat/header.jsx
+++ src/pageFormat/header.jsx
@@ -7,6 +7,8 @@ export default function GlobalHeader() {
         <nav className="nav" aria-label="주요 메뉴">
           <Link to="/home">게임소개</Link>
           <Link to="/sic-blast">플레이!</Link>
+          <Link to="https://github.com/tabthedev/sicHighSchool260716-proj"><img src="/GitHub_Invertocat_White.svg" alt="프로젝트 리포지토리" style={{height:"16px", filter: "brightness(40%)"}}/></Link>
+          
         </nav>
       </header>
     </>
