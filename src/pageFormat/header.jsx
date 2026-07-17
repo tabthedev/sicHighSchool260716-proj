22fed42c794eea5ef1f07b9a7f1fa1eecaa87d85
탭・TAB・タブ <me@tabthedev.kr>
2026-07-17 19:35:35 +0900

    소스코드 글씨 추가

--- src/pageFormat/header.jsx
+++ src/pageFormat/header.jsx
@@ -7,7 +7,7 @@ export default function GlobalHeader() {
         <nav className="nav" aria-label="주요 메뉴">
           <Link to="/home">게임소개</Link>
           <Link to="/sic-blast">플레이!</Link>
-          <Link to="https://github.com/tabthedev/sicHighSchool260716-proj"><img src="/GitHub_Invertocat_White.svg" alt="프로젝트 리포지토리" style={{height:"16px", filter: "brightness(40%)"}}/></Link>
+          <Link to="https://github.com/tabthedev/sicHighSchool260716-proj"><img src="/GitHub_Invertocat_White.svg" alt="프로젝트 리포지토리" style={{height:"16px", filter: "brightness(40%)"}}/> 소스코드</Link>
           
         </nav>
       </header>
